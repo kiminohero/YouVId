4d043a41a8b6cda95778cc8263d8af6c5657d384
kiminohero <bishal5058prime@gmail.com>
2019-03-28 03:10:15 +0530

    Push app to github

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "YOUR YOUTUBE API HERE";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResult: 5,
+    key: KEY
+  }
+});
